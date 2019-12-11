35bcbd82521e0d014dc9c7363625e3133cad7415
sarfios20 <david-hpf@hotmail.com>
2019-12-11 16:34:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+$( function() {
+    $( ".sec" ).sortable();
+    $( ".connectedSortable" ).sortable({
+       items: "li:not(.ui-state-disabled)"
+    });
+    $( ".connectedSortable" ).sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+  } );
+
+
+
+function generate(){
+	var count = $("#listas").length;
+	$("#listas").each(function(){
+		g='<li>\
+    	<ul id="sortable3" class="connectedSortable">\
+    	<h2 class="ui-state-disabled">Gobernar</h2>\
+      	<li class="ui-state-default">'+ $(this).attr('id')+'</li>\
+    	</ul>\
+  		</li>';
+  		console.log(g);
+  		$(document.body)[0].insertAdjacentHTML('beforeend', html);
+  });
+}
+
+$(document).ready(function(){
+  $(".editable").bind("dblclick", replaceHTML);
+  generate();
+});
+
+function nuevoPartido() {
+  console.log("crear listas");
+  console.log("añadirlas");
+  console.log("reset listas anteriores");
+
+  var count = $(".PartidoContainer").length;
+  console.log(count)
+  count = count + 1;
+  //html = '<div class="PartidoContainer" id="Partido'+count+'">Partido'+count+'</div>';
+
+
+  html = '<div class="PartidoContainer" id=Partido'+count+'>\
+  <h2 class="PartidoName editable">Partido'+count+'</h2>\
+  <ul id="sortable1" class="sec">\
+  <li>\
+    <ul id="sortable3" class="connectedSortable">\
+      <h2 class="ui-state-disabled">seccion1</h2>\
+      <li class="ui-state-default">Item 1</li>\
+      <li class="ui-state-default">Item 2</li>\
+      <li class="ui-state-default">Item 3</li>\
+      <li class="ui-state-default">Item 4</li>\
+      <li class="ui-state-default">Item 5</li>\
+    </ul>\
+  </li>\
+  <li>\
+    <ul id="sortable3" class="connectedSortable">\
+      <h2 class="ui-state-disabled">seccion2</h2>\
+      <li class="ui-state-default">Item 1</li>\
+      <li class="ui-state-default">Item 2</li>\
+      <li class="ui-state-default">Item 3</li>\
+      <li class="ui-state-default">Item 4</li>\
+      <li class="ui-state-default">Item 5</li>\
+    </ul>\
+  </li>\
+  <li>\
+    <ul id="sortable3" class="connectedSortable">\
+      <h2 class="ui-state-disabled">seccion3</h2>\
+      <li class="ui-state-default">Item 1</li>\
+      <li class="ui-state-default">Item 2</li>\
+      <li class="ui-state-default">Item 3</li>\
+      <li class="ui-state-default">Item 4</li>\
+      <li class="ui-state-default">Item 5</li>\
+    </ul>\
+  </li>\
+  <li>\
+    <ul id="sortable3" class="connectedSortable">\
+      <h2 class="ui-state-disabled">seccion4</h2>\
+      <li class="ui-state-default">Item 1</li>\
+      <li class="ui-state-default">Item 2</li>\
+      <li class="ui-state-default">Item 3</li>\
+      <li class="ui-state-default">Item 4</li>\
+      <li class="ui-state-default">Item 5</li>\
+    </ul>\
+  </li>\
+  <li>\
+    <ul id="sortable3" class="connectedSortable">\
+      <h2 class="ui-state-disabled">seccion5</h2>\
+      <li class="ui-state-default">Item 1</li>\
+      <li class="ui-state-default">Item 2</li>\
+      <li class="ui-state-default">Item 3</li>\
+      <li class="ui-state-default">Item 4</li>\
+      <li class="ui-state-default">Item 5</li>\
+    </ul>\
+  </li>\
+</ul>\
+</div>';
+
+
+
+  $(document.body)[0].insertAdjacentHTML('beforeend', html);
+  $( function() {
+    $( ".sec" ).sortable();
+    $( ".connectedSortable" ).sortable({
+       items: "li:not(.ui-state-disabled)"
+    });
+    $( ".connectedSortable" ).sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+  } );
+	//var d1 = document.getElementById('one');
+	//d1.insertAdjacentHTML('beforeend', '<div id="two">two</div>');
+
+}
+
+function replaceHTML(){
+        oldText = $(this).html().replace(/"/g, "");
+        $(this).html("").html("<form><input type=\"text\" class=\"editBox\" value=\"" + oldText + "\" /> </form><a href=\"#\" class=\"btnSave\">Save changes</a> <a href=\"#\" class=\"btnDiscard\">Discard changes</a>");
+}
+
+$(document).on("click", ".btnSave",
+    function()
+    {
+        newText = $(this).siblings("form")
+                         .children(".editBox")
+                         .val().replace(/"/g, "");
+                          
+        $(this).parent()
+               .html(newText);
+    }
+);
