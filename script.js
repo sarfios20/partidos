7199fe630e01083be35df5cd97d09477f7786be3
sarfios20 <david-hpf@hotmail.com>
2020-03-03 19:43:26 +0100

    refactoring

--- script.js
+++ script.js
@@ -6,209 +6,73 @@ $( function() {
   $( ".connectedSortable" ).sortable({
     connectWith: ".connectedSortable"
   }).disableSelection();
+
+  update();
 });
 
-function python() {
-	$.ajax({
-		url: "partidos.php",
-		type: "post",
-//		data: {id: ID} ,
-        success: function (response) {
-			console.log(response)
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-           //console.log(textStatus, errorThrown);
-        }
+function appendParties(partido, clase, listas){
+  seccion = $('#'+partido+" ."+clase);
 
-    });
-}
-
-function generate(){
-	var count = $("#listas").length;
-	$("#listas").each(function(){
-		g='<li>\
-    	<ul id="sortable3" class="connectedSortable">\
-    	<h2 class="ui-state-disabled">Gobernar</h2>\
-      	<li class="ui-state-default">'+ $(this).attr('id')+'</li>\
-    	</ul>\
-  		</li>';
-  		$(document.body)[0].insertAdjacentHTML('beforeend', html);
-  });
-}
-
-$(document).ready(function(){
- 	$(".editable").bind("dblclick", replaceHTML);
-
- 	 update();
-});
-
-function GobiernoAmigo(partido, lista){
-  console.log("---"+partido);
-
-  amigos = lista.slice(0, lista.indexOf("Elecciones"));
-  listaSinR = niRojosNiElecciones(partido);
-  seccion = $('#'+partido+" .Amigo");
-
-  amigos.forEach(function( a ) {
-    l = listaSinR.slice();
-    l.splice(partido, 1);
-    hay = niRojosNiElecciones(partido, l);
-    //hay.splice(a, 1);
-    console.log("-"+a);
-    console.log(hay);
-
-    console.log("++++++");
-    console.log(desarrollar(hay));
-
-  });
-
-
-}
-
-function formarCoalicion(partido, lista){
-
-  lista = desarrollar(lista);
-
-  listaPartidos = $('#listas [data-name="'+partido+'"').map(function() {
-    return $(this).text();
-  }).get();
-
-  seccion = $('#'+partido+" .Coalicion");
-
-  lista.forEach(function( miembro ) {
-    item = $('<li class="ui-state-default contenedorElementos"></li>');
-    miembro.forEach(function( p ) {
+  listas.forEach(function( lista ) {
+    contenedor = $('<li class="ui-state-default contenedorElementos"></li>');
+    lista.forEach(function( p ) {
       e = $('<div class="partido" data-name="'+p+'">'+p+'</div>');
-      item.append(e);
+      contenedor.append(e);
     });
-    main = $('<div class="partido" data-name="'+partido+'">'+partido+'</div>');
-    item.append(main);
-    seccion.append(item);
+    //main = $('<div class="partido" data-name="'+partido+'">'+partido+'</div>');
+    seccion.append(contenedor);
   });
 }
 
 function Gobernar(partido, lista){
-/*
-  lista = desarrollar(lista);
+  listas = desarrollar(lista);
 
-	seccion = $('#'+partido+" .Gobierno");
-	e = $('<li class="ui-state-default contenedorElementos"></li>');
-	solo = $('<div class="partido" data-name="'+partido+'">'+partido+'</div>');
-	e.append(solo);
-	seccion.append(e);
-	lista.forEach(function( miembro ) {
-		item = $('<li class="ui-state-default contenedorElementos"></li>');
-		main = $('<div class="partido" data-name="'+partido+'">'+partido+'</div>');
-		item.append(main);
-		miembro.forEach(function( partido ) {
-			p = $('<div class="partido" data-name="'+partido+'">'+partido+'</div>');
-			item.append(p);
-  		});
-		seccion.append(item);
-  });*/
-  appendParties(partido, "Gobierno", desarrollar(lista));
+  listas.forEach(function( l ) {
+      l.unshift(partido);
+  });
 
+  listas.unshift([partido]);
 
-
+  appendParties(partido, "Gobierno", listas);
 }
 
-function appendParties(partido, clase, listas){
-  seccion = $('#'+partido+" ."+clase);
-  console.log(listas)
-  listas.forEach(function( lista ) {
-    item = $('<li class="ui-state-default contenedorElementos"></li>');
-    listas.forEach(function( p ) {
-      esto = $('<div class="partido" data-name="'+p+'">'+p+'</div>');
-      item.append(esto);
-    });
-    seccion.append(esto);
+function formarCoalicion(partido, lista){
+  listas = desarrollar(lista);
+
+  listas.forEach(function( l ) {
+      l.push(partido);
   });
+
+  appendParties(partido, "Coalicion", listas);
+}
+
+function GobiernoAmigo(partido, lista){
+  listaAmigos = lista.slice(0, lista.indexOf('Elecciones'));
+  listas = desarrollar(listaAmigos);
+
+  appendParties(partido, "Amigo", listas);
 }
 
 function update(){
-	seccion = $('.PartidoContainer').children().remove();
+  seccion = $('.PartidoContainer').children().remove();
 
-	listaPartidos = $('#listas h2').map(function() {
+  listaPartidos = $('#listas h2').map(function() {
     return $(this).text();
-	}).get();
+  }).get();
 
   listaPartidos.forEach(function( partido ) {
     nuevoPartido(partido);
     sinRojosE = niRojosNiElecciones(partido);
-    sinRojo = sinRojos(partido);
+    sinRojos = getSinRojos(partido);
+
     Gobernar(partido, sinRojosE);
     formarCoalicion(partido, sinRojosE);
-    GobiernoAmigo(partido, sinRojo);
+    GobiernoAmigo(partido, sinRojos);
   });
-}
-
-function replaceHTML(){
-    oldText = $(this).html().replace(/"/g, "");
-    $(this).html("").html("<form><input type=\"text\" class=\"editBox\" value=\"" + oldText + "\" /> </form><a href=\"#\" class=\"btnSave\">Save changes</a> <a href=\"#\" class=\"btnDiscard\">Discard changes</a>");
-}
-
-function change(elemt){
-    console.log(elemt);
-
-    if (elemt.style.color != "red"){
-    	elemt.style.color = "red";
-    }else{
-    	elemt.style.color = "black";
-    }
-}
-
-$(document).on("click", ".btnSave",
-    function()
-    {
-    	var msglist = $(this).parent();
-
-		var show = msglist.data("name");
-
-        newText = $(this).siblings("form")
-                         .children(".editBox")
-                         .val().replace(/"/g, "");
-                          
-        $(this).parent()
-               .html(newText);
-        lel = "."+show;
-
-        $('[data-name="'+show+'"]').html(newText);
-        $('[data-name="'+show+'"]').attr('data-name',newText); 
-        console.log($('[data-name="'+show+'"]'));
-    }
-);
-
-function aux(partido){
-  lista = listaPartido(partido);
-
-  listaSinRojos = lista.filter(function( index ) {
-    return $(this).css('color') === 'rgb(0, 0, 0)'
-  });
-  return listaSinRojos;
-}
-
-function aux2(lista){
-  listaArray = [];
-  lista.each(function( index ) {
-    listaArray.push($(this).text());
-  });
-  return listaArray;
-}
-
-function sinRojos(partido){
-
-  listaSinRojos = aux(partido);
-
-  listaArray = aux2(listaSinRojos);
-
-
-
-  listaArray.splice(partido, 1);
-
-  return listaArray;
 }
 
 function desarrollar(listaArray){
+
   result = [];
   lorol = [];
 
@@ -216,37 +80,53 @@ function desarrollar(listaArray){
 
   for (i = 1; i < listaArray.length+1; i++) {  
     result.length = i; //n=2
-    combine(listaArray, result.length, 0, result);
-  }
+    forma(listaArray, result.length, lorol);
+  }  
 
   lorol.forEach(function( string ) {
     hey.push(string.split(","));
   });
 
   return hey;
-
 }
 
-function niRojosNiElecciones(partido){
-  listaSinRojos = aux(partido);
-
-  listaArray = aux2(listaSinRojos);
-  listaArray.splice(partido, 1);
-
-	listaArray.splice(listaArray.indexOf('Elecciones'), 1);
-
-	return listaArray;
+function forma(input, len, listaA){
+  combine(input, len, 0, result, listaA);
 }
 
-function combine(input, len, start, result, p) {
+
+function combine(input, len, start, result, listaA) {
   if(len === 0) {
-    lorol.push(result.join(","));
+    listaA.push(result.join(","));
     return;
   }
   for (let i = start; i <= input.length - len; i++) {
     result[result.length - len] = input[i];
-    combine(input, len-1, i+1, result);
+    combine(input, len-1, i+1, result, listaA);
   }
+}
+
+function niRojosNiElecciones(partido){
+  listaSinRojos = getSinRojos(partido);
+
+  listaSinRojos.splice(listaSinRojos.indexOf('Elecciones'), 1);
+
+  return listaSinRojos;
+}
+
+function getSinRojos(partido){
+  lista = listaPartido(partido);
+
+  listaSinRojos = lista.filter(function( index ) {
+    return $(this).css('color') === 'rgb(0, 0, 0)'
+  });
+  
+  listaArray = [];
+  listaSinRojos.each(function( index ) {
+    listaArray.push($(this).text());
+  });
+  listaArray.splice(partido, 1);
+  return listaArray;
 }
 
 function listaPartido(partido){
@@ -255,6 +135,8 @@ function listaPartido(partido){
 
     return lista;
 }
+
+
 
 function nuevoPartido(partido) {
 
@@ -301,8 +183,8 @@ function nuevoPartido(partido) {
   
   nuevo = $(html);
 
- $('.PartidoContainer').append(nuevo);
-  $( function() {
+  $('.PartidoContainer').append(nuevo);
+  $( function(){
     $( ".sec" ).sortable();
     $( ".connectedSortable" ).sortable({
        items: "li:not(.ui-state-disabled)"
@@ -310,5 +192,5 @@ function nuevoPartido(partido) {
     $( ".connectedSortable" ).sortable({
       connectWith: ".connectedSortable"
     }).disableSelection();
-  } );
+  });
 }
